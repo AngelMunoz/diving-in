efde4e6fde13bc8050a77576f20a772d2d8bdfb0
Angel D. Munoz <scythes.lol@gmail.com>
2023-07-23 09:13:33 -0600

    Let's go!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Diving In!
+
+This is going to be a playground for me to learn both ktor and kotlin.
+
+Don't expect anything from it.
---
+++ src/main/kotlin/com/github/angelmunoz/Application.kt
@@ -0,0 +1,21 @@
+package com.github.angelmunoz
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.github.angelmunoz.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureTemplating()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
